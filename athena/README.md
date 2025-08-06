45d110dea2bd83bdcb4d3eadd557bbf7911aac7a
gustrb <gustavoreisbauer@gmail.com>
2025-08-05 21:43:02 -0300

    feat: add simple build script

--- athena/README.md
+++ athena/README.md
@@ -3,3 +3,17 @@
 
 A shit C compiler that I want to base my whole stack upon :)
 
+## Getting started
+
+To start coding you need to bootstrap the compiler, I am currently using no build system since I haven't coded one yet. To build the build script you can run the following command (Unix-only).
+
+```sh
+$ gcc build.c -Wall -Wextra -pedantic
+```
+
+Then you can just run the following command to build everything:
+
+```sh
+$ ./a.out
+```
+
---
+++ athena/build.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+
+#define E_FAILSTRTIME    1
+#define E_FAILDISTCREATE 2
+
+#define LOG_LEVEL_INFO  0
+#define LOG_LEVEL_ERROR 1
+
+static char datebuf[64] = {0};
+
+#define LOG(level, ...) \
+	do { \
+		int err; \
+		if ((err = __refresh_date())) return err; \
+		switch (level) \
+		{ \
+			case LOG_LEVEL_INFO:  { fprintf(stdout, "[%s INFO]: ", datebuf); fprintf(stdout, __VA_ARGS__); }; break;  \
+			case LOG_LEVEL_ERROR: { fprintf(stderr, "[%s ERROR]: ", datebuf); fprintf(stderr, __VA_ARGS__); }; break; \
+		} \
+	} while(0); \
+
+static const char *distfile = "dist";
+static const char *executable_path = "./src/athena.c";
+
+static char *build_command_args = 
+	"gcc "
+	"./src/athena.c "
+	"-o "
+	"./dist/athena "
+	"-Wall "
+	"-Wextra "
+	"-pedantic";
+
+static int __refresh_date(void);
+static int __build_executable(void);
+
+int main(void)
+{
+	struct stat st = {0};
+	LOG(LOG_LEVEL_INFO, "Checking if \"%s\" folder exists...\n", distfile);
+	if (stat(distfile, &st) == -1)
+	{
+		LOG(LOG_LEVEL_INFO, "\"%s\" folder does not exist... Creating it\n", distfile);
+		if (mkdir(distfile, 0700))
+		{
+			LOG(LOG_LEVEL_ERROR, "Failed to create \"%s\" file, exiting out...\n", distfile);
+			return E_FAILDISTCREATE;
+		}
+
+	}
+	else
+	{
+		LOG(LOG_LEVEL_INFO, "\"%s\" folder exists. Skipping creation\n", distfile);
+	}
+
+	int err;
+	if ((err = __build_executable()))
+	{
+		LOG(LOG_LEVEL_ERROR, "Failed to build executable at: \"%s\" exiting out...\n", executable_path);
+		return err;
+	}
+
+
+	LOG(LOG_LEVEL_INFO, "Finished building the app\n");
+	
+	return 0;
+}
+
+static int __refresh_date(void)
+{
+	time_t t = time(NULL);
+	struct tm *tm = localtime(&t);
+	size_t ret = strftime(datebuf, sizeof(datebuf), "%c", tm);
+	if (!ret)
+	{
+		return E_FAILSTRTIME;
+	}
+
+	return 0;
+}
+
+static int __build_executable(void)
+{
+	LOG(LOG_LEVEL_INFO, "Running %s to build the project\n", build_command_args);
+	if (system(build_command_args)) return 1;
+	return 0;
+}
+
